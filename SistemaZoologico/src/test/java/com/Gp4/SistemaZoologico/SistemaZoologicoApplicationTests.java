6557f57a2f111d65666eec30812b337692706305
Daniel Oliveira de Freitas <79385757+Daniel-Oliveira-de-Freitas@users.noreply.github.com>
2021-11-15 15:57:47 -0300

    Add files via upload
    add esqueleto spring do programa

---
+++ SistemaZoologico/src/test/java/com/Gp4/SistemaZoologico/SistemaZoologicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gp4.SistemaZoologico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaZoologicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
